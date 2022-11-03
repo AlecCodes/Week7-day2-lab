a8b9989dded6be4d02df3dd0bf2a256018d1a1ae
Alec Hannaford <alec322@gmail.com>
2022-11-02 20:35:55 -0700

    added tip calculator

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.get('/greeting/:name', (req,res) => {
     res.send(`Hello, ${req.params.name}`)
 })
 
+app.get('/tip/:total/:tipPercentage', (req,res) => {
+
+    const total = req.params.total
+    const Percentage = req.params.tipPercentage 
+    const answer = (total * Percentage)
+    res.send(`Your tip is: $` +  answer)
+})
+
 //////////////////////////////////////////
 //Server listener
 //////////////////////////////////////////
